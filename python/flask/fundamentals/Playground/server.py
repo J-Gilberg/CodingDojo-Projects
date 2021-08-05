f37546ca4c20e34afbf3fad2e354e1d8a13fcaaf
J-Gilberg <85514341+J-Gilberg@users.noreply.github.com>
2021-08-05 16:56:05 -0500

    update

--- python/flask/fundamentals/Playground/server.py
+++ python/flask/fundamentals/Playground/server.py
@@ -1,11 +1,18 @@
 from flask import Flask, render_template
-
 app = Flask(__name__)  
 
 @app.route('/play')    
-def play():
+def play1():
+    return render_template('index.html', element_color = 'blue', times=3) 
 
-    return render_template('index.html') 
+@app.route('/play/<int:x>')    
+def play2(x):
+    return render_template('index.html', element_color = 'blue', times=x) 
+
+@app.route('/play/<int:x>/<color>')    
+def play3(color,x):
+    return render_template('index.html', element_color = color, times=x) 
 
 if __name__=="__main__":     
     app.run(debug=True)
+
