74835ad139a3d057013e39a9cfb6c10205b0bfaa
J-Gilberg <85514341+J-Gilberg@users.noreply.github.com>
2021-08-16 14:02:10 -0500

    update

---
+++ python/flask_mysql/validation/LoginAndReg/flask_app/controllers/messages.py
@@ -0,0 +1,3 @@
+from flask_app import app
+from flask_app.models import loginReg
+from flask import render_template, redirect, request, session, flash
---
+++ python/flask_mysql/validation/LoginAndReg/flask_app/models/message.py
@@ -0,0 +1,2 @@
+from flask_app.config.conn import connectToMySQL
+from flask import flash
