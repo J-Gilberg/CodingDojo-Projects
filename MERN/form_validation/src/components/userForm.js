5f4ce98bf0ad110a04ece99675f47c3f6da8620b
J-Gilberg <85514341+J-Gilberg@users.noreply.github.com>
2021-09-03 10:09:34 -0500

    update

---
+++ MERN/form_validation/src/components/userForm.js
@@ -0,0 +1,17 @@
+import React, { useReducer } from "react";
+
+const UserFrom = () => {
+
+
+    return(
+        <div>
+            <form action="">
+                <input type="text" name="" className=""/>
+                <input type="text" name="" className=""/>
+                <input type="email" name="" className=""/>
+            </form>
+        </div>
+
+    )
+}
+export default UserFrom;
