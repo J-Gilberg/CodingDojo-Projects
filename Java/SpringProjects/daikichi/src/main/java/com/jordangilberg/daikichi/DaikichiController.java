8a04b4533f5e29cfee5c193f9f5dfacc877603f9
J-Gilberg <85514341+J-Gilberg@users.noreply.github.com>
2021-10-03 20:00:42 -0500

    update

--- Java/SpringProjects/daikichi/src/main/java/com/jordangilberg/daikichi/DaikichiController.java
+++ Java/SpringProjects/daikichi/src/main/java/com/jordangilberg/daikichi/DaikichiController.java
@@ -1,6 +1,7 @@
 package com.jordangilberg.daikichi;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,4 +22,18 @@ public class DaikichiController {
 	public String Tomorrow() {
 		return "Tomorrow, an opportunity will arise so be sure to be open to new ideas";
 	}
+	
+	@RequestMapping("/travel/{destination}")
+	public String travel(@PathVariable("destination") String destination) {
+		return "Congratulations! You will soon travel to " + destination;
+	}
+	
+	@RequestMapping("/lotto/{luckyNumber}")
+	public String lotto(@PathVariable("luckyNumber") String luckyNumber) {
+		if(Integer.parseInt(luckyNumber)%2 == 0) {
+			return "You will take a grand journey in the near future, but be weary of tempting offers.";
+		}
+		return "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends.";
+	}
 }
+ 
